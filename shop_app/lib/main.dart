49562b59e44199bb65976664bd343c87eff56321
Dominic Crippa <dominic.crippa@gmail.com>
2021-11-15 23:56:55 +0100

    [ADD] - animations

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import './screens/splash_screen.dart';
 
+import './screens/splash_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -13,6 +13,7 @@ import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,12 +21,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider.value(
-            value: Auth(),
-          ),
-          ChangeNotifierProxyProvider<Auth, Products>(
-            create: (ctx) => Products(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          create: (ctx) => Products(
               "",
               "",
               [],
@@ -33,27 +34,27 @@ class MyApp extends StatelessWidget {
             update: (ctx, auth, previousProducts) => Products(
               auth.token as String,
               auth.userId as String,
-              previousProducts == null ? [] : previousProducts.items,
-            ),
-          ),
-          ChangeNotifierProvider.value(
-            value: Cart(),
-          ),
-          ChangeNotifierProxyProvider<Auth, Orders>(
-            update: (ctx, auth, previousOrders) => Orders(
+                previousProducts == null ? [] : previousProducts.items,
+              ),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+         update: (ctx, auth, previousOrders) => Orders(
               auth.token as String,
               auth.userId as String,
-              previousOrders == null ? [] : previousOrders.orders,
-            ),
-            create: (ctx) => Orders(
+                previousOrders == null ? [] : previousOrders.orders,
+              ),
+              create: (ctx) => Orders(
               "",
               "",
               [],
             ),
-          ),
-        ],
-        child: Consumer<Auth>(
-          builder: (ctx, auth, _) => MaterialApp(
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
               title: 'MyShop',
               theme: ThemeData(
                 primarySwatch: Colors.purple,
@@ -61,13 +62,20 @@ class MyApp extends StatelessWidget {
                   secondary: Colors.deepOrange, // Your accent color
                 ),
                 fontFamily: 'Lato',
+                pageTransitionsTheme: PageTransitionsTheme(
+                  builders: {
+                    TargetPlatform.android: CustomPageTransitionBuilder(),
+                    TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                  },
+                ),
               ),
               home: auth.isAuth
                   ? ProductsOverviewScreen()
                   : FutureBuilder(
                       future: auth.tryAutoLogin(),
-                      builder: (context, snapshot) =>
-                          snapshot.connectionState == ConnectionState.waiting
+                      builder: (ctx, authResultSnapshot) =>
+                          authResultSnapshot.connectionState ==
+                                  ConnectionState.waiting
                               ? SplashScreen()
                               : AuthScreen(),
                     ),
@@ -77,7 +85,9 @@ class MyApp extends StatelessWidget {
                 OrdersScreen.routeName: (ctx) => OrdersScreen(),
                 UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
                 EditProductScreen.routeName: (ctx) => EditProductScreen(),
-              }),
-        ));
+              },
+            ),
+      ),
+    );
   }
 }
